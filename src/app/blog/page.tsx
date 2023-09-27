ade23e0fbe1230f55b68fdf800234b99e4de9202
TalisMan701 <boss.kompaneec@mail.ru>
2023-09-28 01:47:10 +0500

    add ssr in main blog and catalog pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -19,6 +19,8 @@ export const metadata: Metadata = {
   title: 'RSC - блог',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function Blog() {
   const posts = await _getPosts();
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,8 @@ export const metadata: Metadata = {
   title: 'RSC - главная',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const services = await _getPromotedServices();
   return (
